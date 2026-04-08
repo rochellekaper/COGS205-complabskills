bce7485725f3fe7e001f762a1d6406bb434f058d
Rochelle Kaper <rochellka@yahoo.com>
2026-04-08 23:26:54 +0000

    HW2: commit data csv files & script

--- scripts/fetch-csvs.sh
+++ scripts/fetch-csvs.sh
@@ -2,7 +2,7 @@ git pull origin main
 wget 'https://github.com/joachimvandekerckhove/cogs205b-s26/raw/refs/heads/main/modules/02-version-control/files/data.zip'
 unzip data.zip  -d /workspace/COGS205-complabskills/temp
 current_date=$(date +%F)
-cp /workspace/COGS205-complabskills/temp/*.csv /workspace/COGS205-complabskills/data/$current_date/
+cp /temp/*.csv /data/$current_date/
 git add data/
 git add scripts/
 git commit -m "HW2: commit data csv files & script"
