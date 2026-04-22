4e9f45efb482f360ea1259b6c0d29b577c0067a7
Rochelle Kaper <61244351+rochellekaper@users.noreply.github.com>
2026-04-22 15:36:09 -0700

    in class unittesting of sdt in progress

--- scripts/signal_detection.py
+++ scripts/signal_detection.py
@@ -18,15 +18,12 @@ class SignalDetection:
         if hits < 0 or misses < 0 or false_alarms < 0 or correct_rejections < 0:
             raise ValueError("values cannot be negative")   
 
-        #avoid dividing by zero, and taking ppf(0). just make it 1e-9 so almost 0 but not exactly.
-        if self.hits == 0:
-            self.hits = 1e-9
-        if self.misses == 0:
-            self.misses = 1e-9
-        if self.false_alarms == 0:
-            self.false_alarms = 1e-9
-        if self.correct_rejections == 0:
-            self.correct_rejections = 1e-9
+        #avoid dividing by zero, and taking ppf(0)
+        if self.hits == 0 and self.misses == 0:
+            raise ValueError("both hits and misses cannot be 0")   
+        
+        if self.false_alarms == 0 and self.correct_rejections == 0:
+            raise ValueError("both false alarms and correct rejections cannot be 0")  
         
         return 
 
@@ -56,15 +53,14 @@ class SignalDetection:
 
     def plot_sdt(self):
         fig, ax = plt.subplots()
-        mu_signal = 0.8
         mu_noise = 0
         sigma = 1 #same variance
-        x = np.linspace(mu_noise - 4, mu_signal + 4, 1000)
+        x = np.linspace(mu_noise - 4, self.d_prime() + 4, 1000)
         peak = norm.pdf(0, 0, sigma)
-        plt.plot(x, norm.pdf(x, mu_signal, sigma), label = 'Signal', color = 'blue')
+        plt.plot(x, norm.pdf(x, self.d_prime(), sigma), label = 'Signal', color = 'blue')
         plt.plot(x, norm.pdf(x, mu_noise, sigma), label = 'Signal + Noise', color = 'red')
         ax.set_title('Signal to Noise')
-        ax.annotate("d'", xy=(mu_noise, peak), xytext=(mu_signal, peak), 
+        ax.annotate("d'", xy=(mu_noise, peak), xytext=(self.d_prime(), peak), 
                     arrowprops=dict(arrowstyle='<->', color='black', linewidth=2))
 
         ax.axvline(x=self.criterion(), color='black', linestyle='--', linewidth=2, label = "criteron")
@@ -114,4 +110,3 @@ if __name__ == "__main__":
     sdt3 = SignalDetection(4, 6, 31, 8)
     fig2, ax2 = SignalDetection.plot_roc([sdt1, sdt2, sdt3])
     fig2.savefig('roc_plot.png')
-
---
+++ tests/test-signal-detection.py
@@ -0,0 +1,53 @@
+import unittest
+import matplotlib.pyplot as plt
+import copy
+
+class TestSignalDetection(unittest.TestCase):
+    sdt1 = signal_detection.py.SignalDetection(133, 431, 7, 23)
+    def test_init_values_are_numeric(self):
+        self.hits = hits
+        self.misses = misses
+        self.false_alarms = false_alarms
+        self.correct_rejections = correct_rejections
+        attributes = [hits, misses, false_alarms, correct_rejections]
+        for x in attributes:
+             expected = isinstance(x, (int, float))
+             actual = isinstance(x, (int, float))
+                                  
+    def test_init_values_are_nonnegative(self):
+        self.assertTrue(self.hits >= 0)
+        self.assertTrue(self.misses >= 0)
+        self.assertTrue(self.false_alarms >= 0)
+        self.assertTrue(self.correct_rejections >= 0)
+
+    def test_add(self, other):
+        sdt_copy = copy.deepcopy(self)
+        sdt_add = self.hits + self.misses
+        self.assertEqual(sdt_add.hits, sdt_copy.hits, "self.hits was mutated")
+        self.assertEqual(sdt_add.misses, sdt_copy.misses, "self.misses was mutated")
+        return
+    
+    def test_sub(self, other):
+        sdt_copy = copy.deepcopy(self)
+        sdt_sub = self.hits - self.misses
+        self.assertEqual(sdt_sub.hits, sdt_copy.hits, "self.hits was mutated")
+        self.assertEqual(sdt_sub.misses, sdt_copy.misses, "self.misses was mutated")
+        return
+    
+    def test_multi(self, factor):
+        sdt_copy = copy.deepcopy(self)
+        sdt_multi = self * factor
+        self.assertEqual(sdt_multi.hits, sdt_copy.hits, "self.hits was mutated")
+        self.assertEqual(sdt_multi.misses, sdt_copy.misses, "self.misses was mutated")
+
+        return
+    
+
+    def check_plot_sdt(self):
+        fig, ax = sdt1.plot_sdt()
+        self.assertIsInstance(ax, plt.Axes)
+        self.assertEqual(ax.get_xlabel(), 'False Alarm Rate')
+        self.assertEqual(ax.get_ylabel(), 'Hit Rate')
+
+    
+
